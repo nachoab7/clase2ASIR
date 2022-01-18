91a3eb1aa9a20e4a69b2945fac3b90eea9bec8ae
Nacho <ignacioab7@gmail.com>
2022-01-18 13:17:03 +0100

    Me voy a quedar calvo

--- pruebas/tareavuelos/1.php
+++ pruebas/tareavuelos/1.php
@@ -1,4 +1,20 @@
 <?php 
 #php del documento 1
+#Variables
+include "funciones.php";
+$texto=$_POST["texto"];
+$selector=$_POST["vuelos"];
+
+switch ($selector) {
+    case 'media_pasajeros':
+       funcionlaqsea($variable1)
+        break;
+    
+    default:
+        # code...
+        break;
+}
+
+
 
 ?>
--- pruebas/tareavuelos/arraysbd.php
+++ pruebas/tareavuelos/arraysbd.php
@@ -1,5 +1,67 @@
 <?php 
 
 #~posible manera es con un include de todos los php anteriores (son arrays asociativos)
+$arrayciudad=array(
+    "Vueling-AE12"=> "Milán",
+    "Volotea-EA21"=> "Roma",
+    "Iberia-WE44"=> "Estambul",
+    "Ryanair-EW44"=> "Madrid",
+    "Vueling-ZK87"=> "Lima",
+    "Volotea-UA21"=> "Bilbao",
+    "Iberia-WH44"=> "Roma",
+    "Ryanair-EL00"=> "Madrid",
+    "Vueling-AE12"=> "Roma",
+    "Volotea-EA21"=> "Roma"
+);
+
+$arrayfabricante=array(
+    "Vueling-AE12"=>"Boeing",
+    "Volotea-EA21"=>"Airbus",
+    "Iberia-WE44"=>"Airbus",
+    "Ryanair-EW44"=>"Boeing",
+    "Vueling-ZK87 "=>"Airbus",
+    "Volotea-UA21"=>"Boeing",
+    "Iberia-WH44"=>"Boeing",
+    "Ryanair-EL00"=>"Airbus"
+);
+
+$arrayduracion=array(
+    "Vueling-AE12"=>"120 minutos",
+    "Volotea-EA21 "=>"100 minutos",
+    "Iberia-WE44"=>"340 minutos",
+    "Ryanair-EW44"=>"45 minutos",
+    "Vueling-ZK87 "=>"600 minutos",
+    "Volotea-UA21"=>"65 minutos",
+    "Iberia-WH44"=>"90 minutos",
+    "Ryanair-EL00"=>"50 minutos",
+    "Vueling-AE12"=>"115 minutos",
+    "Volotea-EA21"=>"100 minutos",
+    "Iberia-WE44"=>"85 minutos",
+    "Ryanair-EW44"=>"360 minutos"
+);
+
+$arraypasajeros=array(
+    array("Vuelo" => "Vueling-AE12","Personas"=>"90"),
+    array("Vuelo" =>"Volotea-EA21","Personas"=>"75"),
+    array("Vuelo" =>"Iberia-WE44","Personas"=>"100"),
+    array("Vuelo" =>"Ryanair-EW44","Personas"=>"60"),
+    array("Vuelo" =>"Vueling-ZK87","Personas"=>"77"),
+    array("Vuelo" =>"Volotea-UA21","Personas"=>"93"),
+    array("Vuelo" =>"Iberia-WH44","Personas"=>"86"),
+    array("Vuelo" =>"Ryanair-EL00","Personas"=>"75"),
+    array("Vuelo" =>"Vueling-AE12","Personas"=>"87"),
+    array("Vuelo" =>"Volotea-EA21","Personas"=>"97"),
+    array("Vuelo" =>"Iberia-WE44","Personas"=>"81"),
+    array("Vuelo" =>"Ryanair-EW44","Personas"=>"75"),
+    array("Vuelo" =>"Vueling-ZK87","Personas"=>"87"),
+    array("Vuelo" =>"Volotea-UA21","Personas"=>"90"),
+    array("Vuelo" =>"Iberia-WH44","Personas"=>"84"),
+    array("Vuelo" =>"Ryanair-EL00","Personas"=>"67"),
+    array("Vuelo" =>"Vueling-AE12","Personas"=>"86"),
+    array("Vuelo" =>"Volotea-EA21","Personas"=>"66"),
+    array("Vuelo" =>"Iberia-WE44","Personas"=>"40"),
+    array("Vuelo" =>"Ryanair-EW44","Personas"=>"77"),
+    array("Vuelo" =>"Vueling-ZK87","Personas"=>"82")
+);
 
 ?>
